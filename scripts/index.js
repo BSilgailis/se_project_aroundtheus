0981e12d333a9e4734aa126af14a67c3a0152430
Brendan Silgailis <bsilgailis@gmail.com>
2024-06-19 22:12:51 -0700

    Javascript part 4 corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -35,33 +35,44 @@ let profileBioInput = document.querySelector("#bioInput");
 let saveProfile = document.querySelector(".modal__form");
 
 /* Opening and closing of modal */
-openEvent.addEventListener("click", function (event) {
+function fillProfileForm() {
   profileNameInput.value = profileName.textContent;
   profileBioInput.value = profileBio.textContent;
+}
+function modalOpen() {
+  fillProfileForm();
   modDisplay.classList.add("modal_opened");
-});
-closeEvent.addEventListener("click", function (event) {
+}
+function modalClose() {
   modDisplay.classList.remove("modal_opened");
-});
+}
+openEvent.addEventListener("click", modalOpen);
+closeEvent.addEventListener("click", modalClose);
 
 /* Profile Edits */
-saveProfile.addEventListener("submit", function (event) {
+function profileSubmit(event) {
   profileName.textContent = profileNameInput.value;
   profileBio.textContent = profileBioInput.value;
   modDisplay.classList.remove("modal_opened");
   event.preventDefault();
-});
+}
+saveProfile.addEventListener("submit", profileSubmit);
 
-/* Loop for generating cards off template */
-for (i in initialCards) {
+/* function for cloning card */
+function getCardElement(card) {
   /* setting template & elements */
   let cardGallery = document.querySelector(".gallery__container");
   let cardTemplate = document.querySelector("#cardTemplate").content;
   let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   /* setting content of cards */
-  cardElement.querySelector(".card__image").src = initialCards[i].link;
-  cardElement.querySelector(".card__title").textContent = initialCards[i].name;
-  cardElement.querySelector(".card__image").alt = initialCards[i].name;
-  /* appending card into html gallery container */
+  cardElement.querySelector(".card__image").src = initialCards[card].link;
+  cardElement.querySelector(".card__title").textContent =
+    initialCards[card].name;
+  cardElement.querySelector(".card__image").alt = initialCards[card].name;
   cardGallery.append(cardElement);
+}
+/* Loop for generating cards off template function */
+for (card in initialCards) {
+  /* appending card into html gallery container from function */
+  getCardElement(card);
 }
