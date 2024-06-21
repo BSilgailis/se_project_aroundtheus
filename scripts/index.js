7c5aa737518db25268bffee836206a6bb3c5cf34
Brendan Silgailis <bsilgailis@gmail.com>
2024-06-20 21:59:06 -0700

    Javascript part 4 2nd revision

--- scripts/index.js
+++ scripts/index.js
@@ -25,54 +25,55 @@ const initialCards = [
   },
 ];
 /* Elements */
-let modDisplay = document.querySelector(".modal");
-let openEvent = document.querySelector(".profile__edit-btn");
-let closeEvent = document.querySelector(".modal__close-btn");
+const modDisplay = document.querySelector(".modal");
+const openEvent = document.querySelector(".profile__edit-btn");
+const closeEvent = document.querySelector(".modal__close-btn");
 let profileName = document.querySelector(".profile__name");
 let profileBio = document.querySelector(".profile__bio");
 let profileNameInput = document.querySelector("#nameInput");
 let profileBioInput = document.querySelector("#bioInput");
-let saveProfile = document.querySelector(".modal__form");
+const saveProfile = document.querySelector(".modal__form");
+const cardGallery = document.querySelector(".gallery__container");
 
 /* Opening and closing of modal */
 function fillProfileForm() {
   profileNameInput.value = profileName.textContent;
   profileBioInput.value = profileBio.textContent;
 }
-function modalOpen() {
+function openModal() {
   fillProfileForm();
   modDisplay.classList.add("modal_opened");
 }
-function modalClose() {
+function closeModal() {
   modDisplay.classList.remove("modal_opened");
 }
-openEvent.addEventListener("click", modalOpen);
-closeEvent.addEventListener("click", modalClose);
+openEvent.addEventListener("click", openModal);
+closeEvent.addEventListener("click", closeModal);
 
 /* Profile Edits */
-function profileSubmit(event) {
+function submitProfile(event) {
   profileName.textContent = profileNameInput.value;
   profileBio.textContent = profileBioInput.value;
-  modDisplay.classList.remove("modal_opened");
+  closeModal();
   event.preventDefault();
 }
-saveProfile.addEventListener("submit", profileSubmit);
+saveProfile.addEventListener("submit", submitProfile);
 
 /* function for cloning card */
-function getCardElement(card) {
+function getCardElement(data) {
   /* setting template & elements */
-  let cardGallery = document.querySelector(".gallery__container");
-  let cardTemplate = document.querySelector("#cardTemplate").content;
-  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTemplate = document.querySelector("#cardTemplate").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   /* setting content of cards */
-  cardElement.querySelector(".card__image").src = initialCards[card].link;
+  cardElement.querySelector(".card__image").src = initialCards[data].link;
   cardElement.querySelector(".card__title").textContent =
-    initialCards[card].name;
-  cardElement.querySelector(".card__image").alt = initialCards[card].name;
-  cardGallery.append(cardElement);
+    initialCards[data].name;
+  cardElement.querySelector(".card__image").alt = initialCards[data].name;
+  return cardElement;
 }
 /* Loop for generating cards off template function */
 for (card in initialCards) {
   /* appending card into html gallery container from function */
-  getCardElement(card);
+  const cardElement = getCardElement(card);
+  cardGallery.append(cardElement);
 }
